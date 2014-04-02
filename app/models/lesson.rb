208a3ad3a4cc928078f042156c7f7e640ca8f02b
Maureen Dugan & Bonnie Bright <student@epicodus.com>
2014-04-02 11:55:22 -0700

    create next and previous methods for navigation

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,12 @@ class Lesson < ActiveRecord::Base
   validates :name, :presence => true
   validates :content, :presence => true
   validates :number, :uniqueness => true
+
+  def next
+    next_lesson = Lesson.all.detect { |lesson| lesson.number > self.number }
+  end
+
+  def previous
+    previous_lesson = Lesson.all.order(:number).reverse.detect { |lesson| self.number > lesson.number }
+  end
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -4,4 +4,19 @@ describe Lesson do
   it { should validate_presence_of :name }
   it { should validate_presence_of :content }
   it { should validate_uniqueness_of :number }
+
+  context '#next' do
+    it 'returns the lesson with the next-highest number than the current lesson' do
+      current_lesson = Lesson.create({:name => 'lesson1', :number => 1, :content => "small"})
+      next_lesson = Lesson.create({:name => 'lesson2', :number => 3, :content => "small"})
+      current_lesson.next.should eq next_lesson
+    end
+  end
+  context '#previous' do
+    it 'returns the lesson with the next-lowest number than the current lesson' do
+      current_lesson = Lesson.create({:name => 'lesson1', :number => 5, :content => "small"})
+      previous_lesson = Lesson.create({:name => 'lesson2', :number => 4, :content => "small"})
+      current_lesson.previous.should eq previous_lesson
+    end
+  end
 end
